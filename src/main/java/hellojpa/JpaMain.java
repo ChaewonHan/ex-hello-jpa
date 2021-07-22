eefa60b6706b7e44c4154b6e868e5a25e9bb8a2e
ChaewonHan <chwonhan00@gmail.com>
2021-07-22 17:38:41 +0900

    연관관계 매핑 기초

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,42 +4,38 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.*;
+
 
 public class JpaMain {
 
     public static void main(String[] args) {
-        /*EntityManagerFactory는 하나만 생성해서 애플리케이션 전체에서 공유*/
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
-        /*EntityManager는 쓰레드간에 공유X (사용하고 버려야 한다).
-        * DB Connection 같은 거*/
         EntityManager em =  emf.createEntityManager();
 
-        /* JPA의 모든 데이터 변경은 트랜잭션 안에서 실행*/
         EntityTransaction tx = em.getTransaction();
-        tx.begin(); // 트랜젝션 시작
+        tx.begin();
 
         try {
-            Member member1 = new Member();
-            member1.setUsername("A");
+            Team team = new Team();
+            team.setName("TeamB");
+            em.persist(team);
 
-            Member member2 = new Member();
-            member1.setUsername("B");
+            Member member = new Member();
+            member.setUsername("member2");
+            member.changeTeam(team);
+            em.persist(member);
 
-            Member member3 = new Member();
-            member1.setUsername("C");
+//            em.flush();
+//            em.clear();
 
-            System.out.println("==================");
+            Member findMember = em.find(Member.class, member.getId());
+            List<Member> members = findMember.getTeam().getMembers();
 
-            em.persist(member1); // 1, 51번까지 시퀀스 호출
-            em.persist(member2); // 메모리에서 호출
-            em.persist(member3); // 메모리에서 호출
-
-            System.out.println("member1 = " + member1.getId());
-            System.out.println("member2 = " + member2.getId());
-            System.out.println("member3 = " + member3.getId());
-
-            System.out.println("==================");
+            for(Member m : members) {
+                System.out.println("m = " + m.getUsername());
+            }
 
             tx.commit();
         } catch (Exception e) {
